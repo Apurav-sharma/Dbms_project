5a01c8784e7479ecaa926939646a9262423d704c
Apurav <apurav0711@gmail.com>
2025-02-15 16:55:07 +0530

    modified:   app/globals.css 	new file:   app/login/page.js 	modified:   app/page.js 	new file:   database/connectdb.js 	modified:   package-lock.json 	modified:   package.json

---
+++ database/connectdb.js
@@ -0,0 +1,10 @@
+import mysql from "mysql2/promise";
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "IIITV@icd#5560711", 
+  database: "project"
+});
+
+export default db;
