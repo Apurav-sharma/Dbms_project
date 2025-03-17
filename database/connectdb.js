257e580e71858afa9765548955db518c97e0c0e1
Apurav <apurav0711@gmail.com>
2025-03-17 21:20:16 +0530

    modified:   app/Settings/page.js 	modified:   app/api/notification/[email]/route.js 	modified:   app/card/page.js 	modified:   database/connectdb.js

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,15 +1,15 @@
-import mysql from "mysql2/promise"; 
+import mysql from "mysql2/promise";
 import fs from "fs";
 
-const db = mysql.createPool({ 
+const db = mysql.createPool({
   host: "proj.mysql.database.azure.com",
   user: "apurav0711",
   password: "IIITV@icd#5560711",
   database: "project",
   port: 3306,
   ssl: {
-    rejectUnauthorized : true,
-    ca: fs.readFileSync("C:/Users/ARJUN KHATRIYA/Downloads/DigiCertGlobalRootCA.crt.pem"),
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("C:/Users/Apurav/Downloads/DigiCertGlobalRootCA.crt.pem"),
   },
 });
 
